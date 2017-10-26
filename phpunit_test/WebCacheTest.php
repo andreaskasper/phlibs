8acf906444d87c9a9edff4822a188085fdc20469
Andreas Kasper <djassi@web.de>
2017-10-26 17:09:35 +0200

    Create WebCacheTest.php
    Erster Testtest

---
+++ phpunit_test/WebCacheTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ *
+ *
+ *
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+namespace phlibs\Test;
+
+use phlibs\WebCache;
+use PHPUnit\Framework\TestCase;
+/**
+ * PHPMailer - PHP email transport unit test class.
+ */
+final class WebCacheTest extends TestCase {
+	
+	public function testValidate() {
+		$this->assertTrue(true);
+	}
+	
+}
